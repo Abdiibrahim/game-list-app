fc182620c8d620f53bbad4f7688577149d5e15fb
Abdi Ibrahim <abdiibrahim850@gmail.com>
2020-07-08 19:30:03 -0400

    update landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button, Container, Grid } from "@material-ui/core";
 
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Game Tracker</b>
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              A full-stack app that keeps track of video games (in progress)
-            </p>
-            <p>
+      <Container>
+        <Grid container direction="column" alignItems="center" justify="center">
+          <h4>
+            <b>Game Tracker</b>
+          </h4>
+          <p className="flow-text grey-text text-darken-1">
+            A full-stack app that keeps track of video games (in progress)
+          </p>
+          <p>
               <b>TODO: </b>
               <li>create components for titles, user set</li>
               <li>list of games (search / table) (by genre, platform, rating)</li>
@@ -21,36 +21,21 @@ class Landing extends Component {
               <li>game completion status, ratings (plan to, dropped, in progress, completed)</li>
               <li>user info (date joined, number of games/status, average rating, avatar, account settings)</li>
             </p>
-            <br />
-            <div className="col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
-            </div>
-            <div className="col s6">
-              <Link
-                to="/login"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
-              >
-                Log In
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+        </Grid>
+        <br />
+        <Grid container direction="row" alignItems="center" justify="center">
+          <Grid item xs={3} style={{textAlign:"center"}}>
+            <Button href="/register" variant="outlined" color="primary" disableElevation>
+              Register
+            </Button>
+          </Grid>
+          <Grid item xs={3} style={{textAlign:"center"}}>
+            <Button href="/login" variant="contained" color="primary" disableElevation>
+              Login
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     );
   }
 }
